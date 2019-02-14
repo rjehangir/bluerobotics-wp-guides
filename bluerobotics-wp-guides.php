064c5cd1906b4c347c323ae217911384ef2c4927
Rustom Jehangir <rusty@bluerobotics.com>
2019-02-14 00:18:42 +0000

    Update image style.

--- bluerobotics-wp-guides.php
+++ bluerobotics-wp-guides.php
@@ -5,7 +5,7 @@
  * Description: Simple guide post generator for documentation on Blue Robotics website.
  * Author: Rustom Jehangir
  * Author URI: http://rstm.io
- * Version: 0.2.1
+ * Version: 0.2.2
  *
  * Copyright: (c) 2019 Rustom Jehangir
  *
